599776c4171b936b4d7b9dd42b0143da2ae3471e
Christian Dean Demesa <christiandeandemesa@gmail.com>
2021-10-19 18:03:57 -0700

    christian 10/19 commit

--- flask_app/controllers/seasons.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, redirect, request, session, flash
-from flask_app import app
-from flask_app.models.episode import Episode
-from flask_app.models.season import Season
-from flask_app.models.show import Show
-from flask_app.models.user import User
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt(app)
